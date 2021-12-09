ae58de0e1dc6455439f72bea85253fd966b9c7f3
Santeri <santeri.salonen@gmail.com>
2021-12-09 12:23:51 +0200

    add original loc datalayer

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,26 @@
  */
 
 // You can delete this file if you're not using it
+// Import React so that you can use JSX in HeadComponents
+const React = require("react")
+
+const HeadComponents = [
+  <script key="gtm-orig-loc" dangerouslySetInnerHTML={{__html:`
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        originalLocation: document.location.protocol + '//' +
+                          document.location.hostname +
+                          document.location.pathname +
+                          document.location.search
+      });  
+  `}} />
+]
+
+
+exports.onRenderBody = ({
+  setHeadComponents,
+  setHtmlAttributes,
+  setBodyAttributes
+}, pluginOptions) => {
+  setHeadComponents(HeadComponents)
+}
