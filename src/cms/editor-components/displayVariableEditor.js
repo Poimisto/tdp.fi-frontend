177457154347ccdcac24bf5226d6d0719abc9b5c
Juho Björkman <juho@tdp.fi>
2024-12-16 09:57:34 +0200

    Add the option of displaying variables in heading tags

--- src/cms/editor-components/displayVariableEditor.js
+++ src/cms/editor-components/displayVariableEditor.js
@@ -1,6 +1,16 @@
 const editor = props => {
-  return `<DisplayVariable variableKey="${props.variableKey}" bold={${props.bold}} />`
+  return `<DisplayVariable variableKey="${props.variableKey}" tag="${props.tag}" bold={${props.bold}} />`
 }
+
+const tagOptions = [
+  { label: "Paragraph", value: "p" },
+  { label: "Heading 1", value: "h1" },
+  { label: "Heading 2", value: "h2" },
+  { label: "Heading 3", value: "h3" },
+  { label: "Headign 4", value: "h4" },
+  { label: "Headign 5", value: "h5" },
+  { label: "Headign 6", value: "h6" },
+]
 
 export default {
   // Internal id of the component
@@ -15,15 +25,23 @@ export default {
       name: "variableKey",
       widget: "variableSelect",
     },
+    {
+      label: "Tag",
+      name: "tag",
+      widget: "select",
+      options: tagOptions,
+      default: tagOptions[0],
+    },
     { label: "Bold", name: "bold", widget: "boolean", default: false },
   ],
   // Pattern to identify a block as being an instance of this component
-  pattern: /<DisplayVariable\s+variableKey="([^"]+)"(?:\s+bold=\{([^}]+)\})?\s*\/>/s,
+  pattern: /<DisplayVariable\s+variableKey="([^"]+)"(?:\s+tag="([^"]+)")?(?:\s+bold=\{([^}]+)\})?\s*\/>/is,
   // Function to extract data elements fro mthe regex match
   fromBlock: function (match) {
     return {
       variableKey: match[1],
-      bold: match[2] === undefined ? false : match[2] === "true",
+      tag: match[2] || "p",
+      bold: match[3] === undefined ? false : match[3] === "true",
     }
   },
   // Preview output for this component. Can either be a string or a React component
--- src/components/DisplayVariable.js
+++ src/components/DisplayVariable.js
@@ -3,13 +3,15 @@ import { graphql, useStaticQuery } from "gatsby"
 import theme from "../theme"
 import styled from "styled-components"
 
-const VariableDisplay = styled.p`
-  display: inline;
-  font-family: ${theme.bodyFontFamily};
-  font-weight: ${props => (props.bold ? "bold" : "normal")};
-`
-
-const DisplayVariable = ({ variableKey, bold = false }) => {
+/**
+ * Displays the value of a variable stored in file variables.json
+ * @param {Object} props
+ * @param {string} props.variableKey - Key of the variable to show
+ * @param {string} props.type - HTML tag to display the value in, defaults to "p"
+ * @param {boolean} props.bold - Should text be bold, defaults to false
+ * @returns
+ */
+const DisplayVariable = ({ variableKey, tag = "p", bold = false }) => {
   const data = useStaticQuery(graphql`
     query {
       variablesJson {
@@ -21,7 +23,15 @@ const DisplayVariable = ({ variableKey, bold = false }) => {
       }
     }
   `)
-  
+
+  const VariableDisplay = styled(tag)`
+    display: inline;
+    font-family: ${tag[0] === "h"
+      ? theme.headingFontFamily
+      : theme.bodyFontFamily};
+    font-weight: ${props => (props.bold ? "bold" : "normal")};
+  `
+
   return (
     <VariableDisplay bold={bold}>
       {data.variablesJson.variables.find(item => item.key === variableKey)
