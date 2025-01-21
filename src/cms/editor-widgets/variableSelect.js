04cf8e35d9dce8fd4806cbcd7d5cee426f3d0169
Juho Björkman <juho@tdp.fi>
2025-01-21 10:27:46 +0200

    Add comment to re-trigger Netlify build

--- src/cms/editor-widgets/variableSelect.js
+++ src/cms/editor-widgets/variableSelect.js
@@ -2,6 +2,8 @@ import variableJson from "../../../content/variables/variables.json"
 import CMS from "netlify-cms-app"
 import Immutable from "immutable"
 
+// Extends the Netlify CMS default select widget by populating it with values¨
+// read from variables.json
 const variableSelect = props => {
   const SelectControl = CMS.getWidget("select").control
   const selectProps = { ...props }
